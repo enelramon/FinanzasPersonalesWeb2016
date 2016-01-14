015a972f3981a0982920c5e2680963460f57db92
edwinh150 <edwinh2410@gmail.com>
2016-01-13 23:24:35 -0400

    arreglando script

--- DAL/Scripts/TipoIngreso.sql
+++ DAL/Scripts/TipoIngreso.sql
@@ -2,8 +2,8 @@ create database TipoIngresosScrit
 
 use TipoIngresosScrit
 
-create table TipoIngresos(
-TipodeIngresoId int identity(1,1) primary key,
+create table TiposIngresos(
+TipoIngresoId int identity(1,1) primary key,
 Descripcion varchar(40),
 EsActivo bit
 UsuarioId int References Usuarios(UsuarioId)
