093479162464c7bfed20252ddbda665f34f89c1f
edwinh150 <edwinh2410@gmail.com>
2016-01-13 21:01:45 -0400

    terminando los metodos

--- BLL/TiposdeIngresos.cs
+++ BLL/TiposdeIngresos.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
+using DAL;
 
 namespace BLL
 {
     class TiposdeIngresos : ClaseMaestra
     {
+        ConexionDb Conexion = new ConexionDb();
+
         public int TipoIngresoId { get; set; }
         public string Descripcion { get; set; }
         public bool EsActivo { get; set; }
@@ -28,24 +31,68 @@ namespace BLL
 
         public override bool Editar()
         {
-            throw new NotImplementedException();
+            bool retorno = false;
+
+            try
+            {
+                retorno = Conexion.Ejecutar(string.Format(""));
+            }
+            catch (Exception)
+            {
+
+                retorno = false;
+            }
+
+            return retorno;
         }
 
         public override bool Eliminar()
         {
-            throw new NotImplementedException();
+            bool retorno = false;
+
+            try
+            {
+                retorno = Conexion.Ejecutar(string.Format(""));
+            }
+            catch (Exception)
+            {
+
+                retorno = false;
+            }
+
+            return retorno;
         }
 
         public override bool Insertar()
         {
             bool retorno = false;
 
+            try
+            {
+                retorno = Conexion.Ejecutar(string.Format(""));
+            }
+            catch (Exception)
+            {
 
+                retorno = false;
+            }
+
+            return retorno;
         }
 
         public override DataTable Listado(string Campos, string Condicion, string Orden)
         {
-            throw new NotImplementedException();
+            DataTable dt = new DataTable();
+            try
+            {
+                dt = Conexion.ObtenerDatos("select " + Campos + " from TiposdeIngresos where " + Condicion + " " + Orden);
+            }
+            catch (Exception)
+            {
+                
+            }
+
+            return dt;
         }
     }
 }
