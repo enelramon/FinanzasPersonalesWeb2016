cda662f8fa9b0f8e799e00a6b3a7928c7b57441c
edwinh150 <edwinh2410@gmail.com>
2016-01-13 20:48:07 -0400

    agregando clase Tipos de ingresos

---
+++ BLL/TiposdeIngresos.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace BLL
+{
+    class TiposdeIngresos : ClaseMaestra
+    {
+        public int TipoIngresoId { get; set; }
+        public string Descripcion { get; set; }
+        public bool EsActivo { get; set; }
+        public int UsuarioId { get; set; }
+
+        public TiposdeIngresos()
+        {
+            this.TipoIngresoId = 0;
+            this.Descripcion = "";
+            this.EsActivo = false;
+            this.UsuarioId = 0;
+        }
+
+        public override bool Buscar(int IdBuscado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Editar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Eliminar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Insertar()
+        {
+            bool retorno = false;
+
+
+        }
+
+        public override DataTable Listado(string Campos, string Condicion, string Orden)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
