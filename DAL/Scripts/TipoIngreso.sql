39c58bbf4a53e256eebbe7d30f322117d5b1c787
edwinh150 <edwinh2410@gmail.com>
2016-01-15 14:15:53 -0400

    probando y resolviendo error

--- DAL/Scripts/TipoIngreso.sql
+++ DAL/Scripts/TipoIngreso.sql
@@ -1,7 +1,7 @@
 
 create table TiposIngresos(
 	TipoIngresoId int primary key identity(1,1),
-	Descripcion varchar(40),
+	Descripcion varchar(50),
 	EsActivo bit,
 	UsuarioId int References Usuarios(UsuarioId)
 );
