085c97c9b2f6ede80f6cccc97f6206aaea366061
chrisgenao <crizzghie@gmail.com>
2016-01-17 21:13:00 -0400

    Agregando el Menu a la MasterPage

--- FinanzasPersonalesWeb/Site.Master.designer.cs
+++ FinanzasPersonalesWeb/Site.Master.designer.cs
@@ -13,6 +13,15 @@ namespace FinanzasPersonalesWeb {
     public partial class Site {
         
         /// <summary>
+        /// Head1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlHead Head1;
+        
+        /// <summary>
         /// head control.
         /// </summary>
         /// <remarks>
@@ -29,6 +38,15 @@ namespace FinanzasPersonalesWeb {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Menu1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::FinanzasPersonalesWeb.Include.Menu Menu1;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
