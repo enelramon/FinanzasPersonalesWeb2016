2720dbfc6d8aa18f760777d5aadf101c11fb6271
chrisgenao <crizzghie@gmail.com>
2016-01-17 20:59:43 -0400

    Completando la Clase Usuarios

--- BLL/Usuarios.cs
+++ BLL/Usuarios.cs
@@ -7,43 +7,100 @@ using DAL;
 
 namespace BLL
 {
-    class Usuarios : ClaseMaestra
+    public class Usuarios : ClaseMaestra
     {
+        ConexionDb Conexion = new ConexionDb();
+
         public int UsuarioId { get; set; }
         public string Nombres { get; set; }
         public int TipoUsuarioId { get; set; }
+        public string Usuario { get; set; }
         public string Password { get; set; }
+        public string Email { get; set; }
 
-    public Usuarios()
+        public Usuarios()
         {
             this.UsuarioId = 0;
             this.Nombres = "";
             this.TipoUsuarioId = 0;
+            this.Usuario = "";
             this.Password = "";
+            this.Email = "";
         }
-        public override bool Buscar(int IdBuscado)
+
+        public override bool Buscar(Int32 IdBuscado)
         {
-            throw new NotImplementedException();
+            bool Encontro = false;
+            DataTable dt = new DataTable();
+
+            dt = this.Listado("Nombres, TipoUsuarioId, Usuario, Email", "UsuarioId =" + IdBuscado, "UsuarioId Asc");
+
+            if (dt.Rows.Count > 0)
+            {
+                Encontro = true;
+
+                this.UsuarioId = IdBuscado;
+                this.Nombres = (string)dt.Rows[0]["Nombres"];
+                this.Usuario = (string)dt.Rows[0]["Usuario"];
+                this.Email = (string)dt.Rows[0]["Email"];
+            }
+
+            return Encontro;
         }
+
 
         public override bool Editar()
         {
-            throw new NotImplementedException();
+            Boolean paso = false;
+
+            paso = Conexion.Ejecutar(string.Format("Update Usuarios set Nombres = '{0}', Usuario = '{1}', Contrasena = '{2}', Email = '{3}', TipoUsuarioId = {4}  where UsuarioId = {5}", this.Nombres, this.Usuario, this.Password, this.Email, this.TipoUsuarioId));
+
+            return paso;
         }
 
         public override bool Eliminar()
         {
-            throw new NotImplementedException();
+            bool retorno = false;
+
+            try
+            {
+                retorno = Conexion.Ejecutar(string.Format("delete from Usuarios where UsuarioId = {0}", this.UsuarioId));
+            }
+            catch (Exception)
+            {
+
+                retorno = false;
+            }
+
+            return retorno;
         }
 
         public override bool Insertar()
         {
-            throw new NotImplementedException();
+            bool retorno = false;
+
+            try
+            {
+                retorno = Conexion.Ejecutar(string.Format("insert into Usuarios (Nombres, Usuario, Contrasena, Email, TipoUsuarioId) values ('{0}','{1},'{2}','{3}',{4})", this.Nombres, this.UsuarioId, this.Password, this.Email, this.TipoUsuarioId));
+            }
+            catch (Exception)
+            {
+
+                retorno = false;
+            }
+
+            return retorno;
         }
 
         public override DataTable Listado(string Campos, string Condicion, string Orden)
         {
-            throw new NotImplementedException();
+            string OrdenFinal = "";
+
+            if (!Orden.Equals(""))
+            {
+                OrdenFinal = " Order by " + Orden;
+            }
+            return Conexion.ObtenerDatos("select " + Campos + " from Usuarios where " + Condicion + " " + OrdenFinal);
         }
     }
 }
