5d1f8a2ab2c39821c3154036a7179c2a267bbdd7
chrisgenao <christopher_genao@hotmail.com>
2016-01-20 02:07:22 -0400

    Probando mejoras

--- FinanzasPersonalesWeb/Default.aspx.designer.cs
+++ FinanzasPersonalesWeb/Default.aspx.designer.cs
@@ -13,21 +13,12 @@ namespace FinanzasPersonalesWeb {
     public partial class Default {
         
         /// <summary>
-        /// TextBox1 control.
+        /// Label1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox1;
-        
-        /// <summary>
-        /// TextBox2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+        protected global::System.Web.UI.WebControls.Label Label1;
     }
 }
