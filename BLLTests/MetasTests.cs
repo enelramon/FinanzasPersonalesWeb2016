cb5da01ff6b16f96ad40a4ad682458dd0fa41c18
SurpUser <franciselcaliente@gmail.com>
2016-01-21 14:54:42 +0100

    Agregando interaccion de la clase Metas con la UI

---
+++ BLLTests/MetasTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL.Tests
+{
+    [TestClass()]
+    public class MetasTests
+    {
+        [TestMethod()]
+        public void MetasTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MetasTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AgregarMetasTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LimpiarListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void InsertarTest()
+        {
+            Metas meta = new Metas();
+            meta.Descripcion = "Mi Meta Personal";
+            meta.UsuarioId = 1;
+            meta.AgregarMetas(1, 1, 200);
+            
+            Assert.IsTrue(meta.Insertar());
+        }
+
+        [TestMethod()]
+        public void EditarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ListadoTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
--- FinanzasPersonalesWeb/MetasWebForm.aspx.cs
+++ FinanzasPersonalesWeb/MetasWebForm.aspx.cs
@@ -4,14 +4,114 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BLL;
 
 namespace FinanzasPersonalesWeb
 {
     public partial class MetasWebForm : System.Web.UI.Page
     {
+        Metas meta = new Metas();
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+
+        public void Limpiar()
+        {
+            MetaIdTextBox.Text = "";
+            DescripcionTextBox.Text = "";
+            TipoDeIngresoDropDownList.SelectedIndex=0;
+            MontoTextBox.Text = "";
+            MetasListBox.Items.Clear();
+        }
+
+        protected void GuadarButton_Click1(object sender, EventArgs e)
+        {
+            meta.Descripcion = DescripcionTextBox.Text;
+            meta.UsuarioId = 1;
+            meta.LimpiarList();
+            for (int i = 0; i < MetasListBox.Items.Count; i++)
+            {
+                int id = (int)meta.ObtenerMetaId().Rows[0]["MetaId"];
+                meta.AgregarMetas(id,1,333);
+            }
+
+            if (meta.Insertar())
+            {
+                HttpContext.Current.Response.Write("<SCRIPT>alert('Guardado Correctamente')</SCRIPT>");
+            }
+            else
+            {
+                HttpContext.Current.Response.Write("<SCRIPT>alert('Error al Guardar')</SCRIPT>");
+            }
+        }
+
+        protected void BuscarButton_Click(object sender, EventArgs e)
+        {
+            int Id = 0;
+            int.TryParse(MetaIdTextBox.Text, out Id);
+            if (Id > 0)
+            {
+                if (meta.Buscar(Id))
+                {
+                    DescripcionTextBox.Text = meta.Descripcion;
+                    foreach (var item in meta.metas)
+                    {
+                        MetasListBox.Items.Add(meta.Monto.ToString());
+                    }
+                }
+                else
+                {
+                    HttpContext.Current.Response.Write("<SCRIPT>alert('Id No Existe')</SCRIPT>");
+                }
+            }
+            else
+            {
+                HttpContext.Current.Response.Write("<SCRIPT>alert('Id Incorrecto')</SCRIPT>");
+            }
+            
+        }
+
+        protected void AgregarButton_Click(object sender, EventArgs e)
+        {
+            if (MontoTextBox.Text.Length > 0)
+            {
+                MetasListBox.Items.Add(MontoTextBox.Text);
+                MontoTextBox.Text = "";
+            }
+            else
+            {
+                HttpContext.Current.Response.Write("<SCRIPT>alert('Ingrese un Monto')</SCRIPT>");
+            }
+        }
+
+        protected void EliminarButton_Click(object sender, EventArgs e)
+        {
+            int Id = 0;
+            int.TryParse(MetaIdTextBox.Text, out Id);
+            if (Id > 0)
+            {
+                meta.MetaId = Id;
+                if (meta.Eliminar())
+                {
+                    HttpContext.Current.Response.Write("<SCRIPT>alert('Eliminado Correctamente')</SCRIPT>");
+                    Limpiar();
+                }
+                else
+                {
+                    HttpContext.Current.Response.Write("<SCRIPT>alert('Error al Eliminado')</SCRIPT>");
+                }
+            }
+            else
+            {
+                HttpContext.Current.Response.Write("<SCRIPT>alert('Id Incorrecto')</SCRIPT>");
+            }
+        }
+
+        protected void NuevoButton_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
     }
 }
