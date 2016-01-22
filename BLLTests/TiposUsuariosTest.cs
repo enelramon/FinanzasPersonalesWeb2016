9ee5dcd3e7d54cb7de37991a79edca2c31be30f8
theamanzing <Darlin.Almonte@gmail.com>
2016-01-22 17:47:26 -0400

    Agregando la Clase de TiposUsuarios al Proyecto de Prueba

---
+++ BLLTests/TiposUsuariosTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BLLTests
+{
+    [TestClass]
+    public class TiposUsuariosTest
+    {
+        [TestMethod]
+        public void UsuariosTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void InsertarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ListadoTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
+
