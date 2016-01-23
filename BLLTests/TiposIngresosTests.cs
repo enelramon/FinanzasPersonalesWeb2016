7520b493fdedd799c3ec74949469d327f3099ee7
edwinh150 <edwinh2410@gmail.com>
2016-01-23 19:09:47 -0400

    creando test de tipoingresos

---
+++ BLLTests/TiposIngresosTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL.Tests
+{
+    [TestClass()]
+    public class TiposIngresosTests
+    {
+        [TestMethod()]
+        public void TiposIngresosTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void InsertarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ListadoTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
