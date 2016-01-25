2892ccce2af67bc073ad32d5a8272e4f4badc647
angelrod25 <angelrguez25@gmail.com>
2016-01-25 14:21:55 -0400

    Creando MiembrosTest

---
+++ BLLTests/MiembrosTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL.Tests
+{
+    [TestClass()]
+    public class MiembrosTests
+    {
+        [TestMethod()]
+        public void MiembrosTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void InsertarTest()
+        {
+            Miembros miembro = new Miembros();
+           
+            miembro.Nombre = "Familia";
+            miembro.esActivo = 1;
+            miembro.UsuarioId = 1;
+
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ListadoTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
