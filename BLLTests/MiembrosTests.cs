18d346d588ffd0dd52a00cd95389254bb4576397
angelrod25 <angelrguez25@gmail.com>
2016-01-25 14:44:51 -0400

    Terminando MiembrosTest

--- BLLTests/MiembrosTests.cs
+++ BLLTests/MiembrosTests.cs
@@ -23,33 +23,49 @@ namespace BLL.Tests
            
             miembro.Nombre = "Familia";
             miembro.esActivo = 1;
-            miembro.UsuarioId = 1;
+            miembro.UsuarioId = 5;
 
-            Assert.Fail();
+            Assert.IsTrue(miembro.Insertar());
         }
 
         [TestMethod()]
         public void EditarTest()
         {
-            Assert.Fail();
+            Miembros miembro = new Miembros();
+
+            miembro.MiembroId = 1;
+            miembro.Nombre = "NOmbre de miembro editado";
+            miembro.esActivo = 0;
+            miembro.UsuarioId = 4;
+
+            Assert.IsTrue(miembro.Editar());
         }
 
         [TestMethod()]
         public void EliminarTest()
         {
-            Assert.Fail();
+            Miembros miembro = new Miembros();
+
+            miembro.MiembroId = 2;
+            
+
+            Assert.IsTrue(miembro.Eliminar());
         }
 
         [TestMethod()]
         public void BuscarTest()
         {
-            Assert.Fail();
+            Miembros miembro = new Miembros();
+
+            Assert.IsTrue(miembro.Buscar(1));
         }
 
         [TestMethod()]
         public void ListadoTest()
         {
-            Assert.Fail();
+            Miembros miembro = new Miembros();
+
+            Assert.IsTrue(miembro.Listado(" * ", "1=1", "").Rows.Count > 0);
         }
     }
 }
