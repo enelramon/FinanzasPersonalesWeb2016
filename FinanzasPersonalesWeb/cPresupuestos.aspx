f68a002416cfacc4a5acc89e99bbac3f78eae02f
Newlin Duarte <dnewlin13@hotmail.com>
2016-01-31 22:42:31 -0400

    Cambios a cPresupuestos

---
+++ FinanzasPersonalesWeb/cPresupuestos.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Site.Master" CodeBehind="cPresupuestos.aspx.cs" Inherits="FinanzasPersonalesWeb.cPresupuestos" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+
+<%--    <!DOCTYPE html>
+    
+    <html xmlns="http://www.w3.org/1999/xhtml">
+        <head runat="server">
+            <title></title>
+        
+        </head>
+        <body>
+            <form id="form1" runat="server">
+                <div>
+        
+                </div>
+            </form>
+        </body>
+     </html>--%>
+</asp:Content>  
+
+<asp:Content ID="Content2" runat="server" contentplaceholderid="ContentPlaceHolder1">
+            Buscar por:
+            <asp:DropDownList ID="FiltroDropDownList" runat="server" Height="35px">
+                <asp:ListItem Selected="True" Value="PresupuestoId">Id</asp:ListItem>
+                <asp:ListItem Value="UsuarioId">Usuario</asp:ListItem>
+                <asp:ListItem>Descripcion</asp:ListItem>
+            </asp:DropDownList>
+            <asp:TextBox ID="BuscarTextBox" runat="server" Width="280px"></asp:TextBox>
+            <asp:Button ID="BuscarButton" runat="server" Text="Buscar" OnClick="BuscarButton_Click" />
+            <br />
+            <asp:GridView ID="PresupuestoGridView" runat="server" Width="313px">
+            </asp:GridView>
+ </asp:Content>         
+        
+
+  
--- FinanzasPersonalesWeb/rPresupuestos.aspx.cs
+++ FinanzasPersonalesWeb/rPresupuestos.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BLL;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,12 +13,84 @@ namespace FinanzasPersonalesWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            TiposEgresos tipoEgreso = new TiposEgresos();
+            Usuarios usuario = new Usuarios();
+            DataTable dt = new DataTable();
+            if (IsPostBack == false)
+            {
+                UsuarioDropDownList.DataSource = usuario.Listado(" * ", "1=1", "");
+                UsuarioDropDownList.DataTextField = "Usuario";
+                UsuarioDropDownList.DataValueField = "UsuarioId";
+                UsuarioDropDownList.DataBind();
 
+                TipoEgresoDropDownList.DataSource = tipoEgreso.Listado(" * ", "1=1", "");
+                TipoEgresoDropDownList.DataTextField = "Descripcion";
+                TipoEgresoDropDownList.DataValueField = "TipoEgresoId";
+                TipoEgresoDropDownList.DataBind();
+
+                dt.Columns.AddRange(new DataColumn[2] { new DataColumn("Tipo Egreso"), new DataColumn("Monto") });
+                ViewState["Detalle"] = dt;
+            }
         }
 
-        protected void Button5_Click(object sender, EventArgs e)
+        protected void BindGrid()
         {
+            DetalleGridView.DataSource = (DataTable)ViewState["Detalle"];
+            DetalleGridView.DataBind();
+        }
 
+
+        public void LlenarDatos(Presupuesto presupuesto) {
+            presupuesto.UsuarioId = Convert.ToInt32(UsuarioDropDownList.SelectedValue);
+            presupuesto.Descripcion = DescripcionTextBox.Text;
+            foreach (GridViewRow dr in DetalleGridView.Rows)
+            {
+                presupuesto.AgregarDetalle(Convert.ToInt32(dr.Cells[0].Text), Convert.ToSingle(dr.Cells[1].Text));
+            }
+            
+        }
+
+        protected void AgregarButton_Click(object sender, EventArgs e)
+        {
+            DataTable dt = (DataTable)ViewState["Detalle"];
+            dt.Rows.Add(TipoEgresoDropDownList.SelectedValue, MontoTextBox.Text.Trim());
+            ViewState["Detalle"] = dt;
+            this.BindGrid();
+            MontoTextBox.Text = "";
+        }
+
+        protected void Limpiar() {
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[2] { new DataColumn("Tipo Egreso"), new DataColumn("Monto") });
+
+            PresupuestoTextBox.Text = "";
+            DescripcionTextBox.Text = "";
+
+            ViewState["Detalle"] = dt;
+            this.BindGrid();
+        }
+
+        protected void LimpiarButton_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        protected void GuardarButton_Click(object sender, EventArgs e)
+        {
+            Presupuesto presupuesto = new Presupuesto();
+            LlenarDatos(presupuesto);
+            presupuesto.Insertar();
+            Limpiar();
+        }
+
+        protected void EliminarButton_Click(object sender, EventArgs e)
+        {
+            Presupuesto presupuesto = new Presupuesto();
+            if (presupuesto.Buscar(Convert.ToInt32(PresupuestoTextBox.Text)))
+            {
+                presupuesto.Eliminar();
+            }
+            
         }
     }
 }
