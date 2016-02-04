2931c80d813c00a215510c6bb13d36baea1c8481
Newlin Duarte <dnewlin13@hotmail.com>
2016-02-04 15:09:58 -0400

    Arreglo de script de Presupuesto y PresupuestosDetalle(Hace falta actualizar las tablas para el funcionamiento de la clase)

--- DAL/Scripts/ScriptSQL.sql
+++ DAL/Scripts/ScriptSQL.sql
@@ -83,67 +83,29 @@ UsuarioId int foreign key References Usuarios(UsuarioId)
 
 )
 --Script Newlin
-/** Faltan algunos FK por falta de las otras tablas los agrego cuando se crean las tablas **/
 
 /****** Object:  Table [dbo].[Presupuestos]    Script Date: 1/18/2016 8:54:42 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-SET ANSI_PADDING ON
-GO
-
-CREATE TABLE [dbo].[Presupuestos](
-	[PresupuestoId] [int] IDENTITY(1,1) NOT NULL,
-	[UsuarioId] [int] NULL,
-	[Descripcion] [varchar](100) NULL,
- CONSTRAINT [PK_Presupuesto] PRIMARY KEY CLUSTERED 
-(
-	[PresupuestoId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
 
 GO
+CREATE TABLE Presupuestos(
+	PresupuestoId int PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	UsuarioId int REFERENCES Usuarios(UsuarioId),
+	Descripcion varchar(100)
+)
 
-SET ANSI_PADDING OFF
 GO
 
 
 /****** Object:  Table [dbo].[PresupuestoDetalle]    Script Date: 1/18/2016 9:08:04 PM ******/
-SET ANSI_NULLS ON
+GO
+CREATE TABLE [dbo].[PresupuestosDetalle](
+	PresupuestoDetalleId int PRIMARY KEY IDENTITY(1,1),
+	PresupuestoId int REFERENCES Presupuestos(PresupuestoId) NOT NULL,
+	TipoEgresoId int REFERENCES TiposEgresos(TipoEgresoId),
+	Monto float NOT NULL
+)
 GO
 
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[PresupuestoDetalle](
-	[PresupuestoDetalleId] [int] IDENTITY(1,1) NOT NULL,
-	[PresupuestoId] [int] NOT NULL,
-	[TipoEgresosId] [int] NULL,
-	[Monto] [float] NOT NULL,
- CONSTRAINT [PK_PresupuestoDetalle] PRIMARY KEY CLUSTERED 
-(
-	[PresupuestoDetalleId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-
-GO
-
-ALTER TABLE [dbo].[PresupuestoDetalle]  WITH CHECK ADD  CONSTRAINT [FK_PresupuestoDetalle_Presupuestos] FOREIGN KEY([PresupuestoId])
-REFERENCES [dbo].[Presupuestos] ([PresupuestoId])
-GO
-
-ALTER TABLE [dbo].[PresupuestoDetalle] CHECK CONSTRAINT [FK_PresupuestoDetalle_Presupuestos]
-GO
-
-ALTER TABLE [dbo].[PresupuestoDetalle]  WITH CHECK ADD  CONSTRAINT [FK_PresupuestoDetalle_TiposEgresos] FOREIGN KEY([TipoEgresosId])
-REFERENCES [dbo].[TiposEgresos] ([TipoEgresoId])
-GO
-
-ALTER TABLE [dbo].[PresupuestoDetalle] CHECK CONSTRAINT [FK_PresupuestoDetalle_TiposEgresos]
-GO
 
 --Script Tipo Ingreso
 create table TiposIngresos(
