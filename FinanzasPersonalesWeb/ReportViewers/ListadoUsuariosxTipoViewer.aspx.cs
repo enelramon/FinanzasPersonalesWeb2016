f91140e16845b8f9b60e69b3d5858cea395ebc6b
enelramon@gmail.com <enelramon@gmail.com>
2016-02-03 20:16:48 -0400

    Agregando Viewer

---
+++ FinanzasPersonalesWeb/ReportViewers/ListadoUsuariosxTipoViewer.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.Reporting.WebForms;
+using BLL;
+
+namespace FinanzasPersonalesWeb.ReportViewers
+{
+    public partial class ListadoUsuariosxTipoViewer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Usuarios usuario = new Usuarios();
+
+                RptViewer.ProcessingMode = Microsoft.Reporting.WebForms.ProcessingMode.Local;
+                RptViewer.Reset();
+                RptViewer.LocalReport.ReportPath = Server.MapPath( @"~\Reportes\ListadoUsuariosxTipo.rdlc");
+
+                RptViewer.LocalReport.DataSources.Clear();
+
+                RptViewer.LocalReport.DataSources.Add(new ReportDataSource("DataSet1", usuario.Listado("*","1=1","") ));
+                RptViewer.LocalReport.Refresh();
+
+            }
+        }
+    }
+}
