fd6142cf65449184654e73e807343183a64dcccf
SurpUser <franciselcaliente@gmail.com>
2016-02-03 20:53:37 -0400

    solucionando Errores

--- FinanzasPersonalesWeb/Consultas/cTiposUsuarios.aspx.cs
+++ FinanzasPersonalesWeb/Consultas/cTiposUsuarios.aspx.cs
@@ -6,7 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using BLL;
 using System.Data;
-using DAL;
+
 
 namespace FinanzasPersonalesWeb.Consultas
 {
@@ -14,16 +14,16 @@ namespace FinanzasPersonalesWeb.Consultas
     {
         protected void BuscarPor_Click(object sender, EventArgs e)
         {
-            ConexionDb Conexion = new ConexionDb();
+            //ConexionDb Conexion = new ConexionDb();
             DataTable dt = new DataTable();
-            string Comodin = "";
-            if (OperadorDropDownList.SelectedIndex == 0)
-            {
-                Comodin = "%";
-            }
+            //string Comodin = "";
+            //if (OperadorDropDownList.SelectedIndex == 0)
+            //{
+            //    Comodin = "%";
+            //}
 
 
-            dt = Conexion.ObtenerDatos("Select IdTipoUsuario as Codigo,Descripcion from TiposUsuario Where  " + BuscarDropDownList.SelectedValue + " " + OperadorDropDownList.SelectedValue + " '" + Comodin + FiltroTextBox.Text.Trim() + Comodin + "'");
+            //dt = Conexion.ObtenerDatos("Select IdTipoUsuario as Codigo,Descripcion from TiposUsuario Where  " + BuscarDropDownList.SelectedValue + " " + OperadorDropDownList.SelectedValue + " '" + Comodin + FiltroTextBox.Text.Trim() + Comodin + "'");
 
             GrdDatos.DataSource = dt;
             GrdDatos.DataBind();
