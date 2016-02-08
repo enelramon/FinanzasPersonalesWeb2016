55da78fab67c7a406d87daf174f7fa8db7c82eb6
chrisgenao <christopher_genao@hotmail.com>
2016-02-08 00:15:07 -0400

    No se que hice pero ya hice mi reporte, que funcione, eso es otra cosa..

--- FinanzasPersonalesWeb/ReportViewers/ListadoUsuariosxTipoViewer.aspx.designer.cs
+++ FinanzasPersonalesWeb/ReportViewers/ListadoUsuariosxTipoViewer.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace FinanzasPersonalesWeb.ReportViewers {
     public partial class ListadoUsuariosxTipoViewer {
         
         /// <summary>
-        /// ReportViewer1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::Microsoft.Reporting.WebForms.ReportViewer ReportViewer1;
-        
-        /// <summary>
         /// RptViewer control.
         /// </summary>
         /// <remarks>
