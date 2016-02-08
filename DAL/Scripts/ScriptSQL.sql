04d16911fa562cb39a65468d61d5be1b02a9f25c
YahaidaVargas <lafenix2003@gmail.com>
2016-02-08 16:11:34 -0400

    agregando el script a mano

--- DAL/Scripts/ScriptSQL.sql
+++ DAL/Scripts/ScriptSQL.sql
@@ -18,17 +18,14 @@ Contrasena nvarchar(10),
 Email nvarchar(30))
 select *from Usuarios
 --Script Yahaida
-CREATE TABLE [dbo].[Cuentas](
-	[CuentaId] [int] IDENTITY(1,1) NOT NULL,
-	[UsuarioId] [int] NULL,
-	[Descripcion] [varchar](100) NULL,
-	[Balance] [float] NULL,
-	[Porciento] [int] NULL,
- CONSTRAINT [PK_Cuentas] PRIMARY KEY CLUSTERED 
-(
-	[CuentaId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
+create table Cuentas(
+CuentaId int identity(1,1) primary key,
+UsuarioId int References Usuarios(UsuarioId),
+Descripcion varchar(100),
+Balance float,
+Porciento int
+)
+
 
 
 --Script Melvin
