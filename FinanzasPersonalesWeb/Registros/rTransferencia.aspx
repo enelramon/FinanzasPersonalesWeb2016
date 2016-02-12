0b64a9c7f959285468b510293fcc1e2050c3a547
AlbertPadi <jhoanpadi@gmail.com>
2016-02-12 09:07:40 -0400

    Agregando formulatios registro y consulta de Transferencias

---
+++ FinanzasPersonalesWeb/Registros/rTransferencia.aspx
@@ -0,0 +1,93 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="rTransferencia.aspx.cs" Inherits="FinanzasPersonalesWeb.Transferencia" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style type="text/css">
+        #TextArea1 {
+            height: 65px;
+            width: 197px;
+        }
+        #ObservacionTextArea {
+            height: 33px;
+        }
+        html {
+  font-family: sans-serif;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  background:#bff5c7;
+}
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container">
+        <div class="row">
+        <div class="col-md-2">
+   
+            <h6><asp:Label ID="Label6" runat="server" Text="TransferenciaId"></asp:Label></h6>
+            <br />
+            <h6><asp:Label ID="Label3" runat="server" Text="Fecha"></asp:Label></h6>    
+            <br />
+            <h6><asp:Label ID="Label7" runat="server" Text="Cuenta origen Id"></asp:Label></h6>   
+            <br />
+            <h6><asp:Label ID="Label1" runat="server" Text="Cuenta Destino Id"></asp:Label></h6>
+            <br />
+            <h6><asp:Label ID="Label2" runat="server" Text="Monto"></asp:Label></h6>   
+            <br />
+            <h6><asp:Label ID="Label4" runat="server" Text="Observacion"></asp:Label></h6>   
+            <br />
+            <h6><asp:Label ID="Label5" runat="server" Text="Usuario Id"></asp:Label></h6>   
+          
+        </div>
+        <div class="col-md-2">
+          
+            <asp:TextBox ID="TransferenciaIdTextBox" runat="server"></asp:TextBox>    
+            <br />
+            <br />
+            <br />
+            <asp:TextBox ID="FechaTextBox" runat="server" CssClass="form-class"></asp:TextBox>
+            <br />
+            <br />
+            <asp:TextBox ID="CuentaOrigenTextBox" runat="server" CssClass="form-class"></asp:TextBox>
+            <br />
+            <br />
+            <asp:TextBox ID="CuentaDestinoTextBox" runat="server" CssClass="form-class"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            <asp:TextBox ID="MontoTextBox" runat="server" CssClass="form-class"></asp:TextBox>
+            <br />
+            <br />
+            <asp:TextBox ID="ObservacionTextBox" runat="server" CssClass="form-class"></asp:TextBox>
+            <br />
+            <br />
+            <asp:TextBox ID="UsuarioIdTextBox" runat="server" CssClass="form-class"></asp:TextBox>
+      
+        </div>
+
+        <div class="col-md-2">
+            <asp:Button ID="Buscarbutton" runat="server" Text="Buscar" CssClass="btn btn-info" OnClick="Buscarbutton_Click" />
+        </div>
+            <div class="col-md-4">
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+           <asp:Button ID="Nuevobutton" runat="server" CssClass="btn btn-primary" Text="Nuevo" OnClick="Nuevobutton_Click"/>
+           <asp:Button ID="GuardarButton" runat="server" OnClick="GuardarButton_Click1" CssClass="btn btn-success" Text="Guardar"/>
+           <asp:Button ID="Eliminarbutton" runat="server" Text="Eliminar" CssClass="btn btn-danger" OnClick="Eliminarbutton_Click" />
+
+        </div>
+            </div>
+
+    </div>
+
+        </asp:Content>
---
+++ FinanzasPersonalesWeb/Registros/rTransferencia.aspx.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BLL;
+namespace FinanzasPersonalesWeb
+{
+    public partial class Transferencia : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Guardarbutton_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        public void Limpiar()
+        {
+            FechaTextBox.Text = string.Empty;
+            ObservacionTextBox.Text = string.Empty;
+        }
+
+        protected void GuardarButton_Click1(object sender, EventArgs e)
+        {
+            Transferencias transferencia = new Transferencias();
+
+            if (TransferenciaIdTextBox.Text.Length == 0)
+            {
+
+                transferencia.Fecha = FechaTextBox.Text;
+                transferencia.CuentaOrigenId = Convert.ToInt32(CuentaOrigenTextBox.Text);
+                transferencia.CuentaDestinoId = Convert.ToInt32(CuentaDestinoTextBox.Text);
+                transferencia.Monto = Convert.ToDouble(MontoTextBox.Text);
+                transferencia.Observacion = ObservacionTextBox.Text;
+                transferencia.UsuarioId = Convert.ToInt32(UsuarioIdTextBox.Text);
+                if (transferencia.Insertar())
+                {
+                    HttpContext.Current.Response.Write("<SCRIPT>alert('Transferencia Realizada')</SCRIPT>");
+                    Limpiar();
+                }
+                else
+                {
+                    HttpContext.Current.Response.Write("<SCRIPT>alert('Transferencia no Realizada')</SCRIPT>");
+                }
+            }
+            else
+            {
+                int id;
+                id = Convert.ToInt32(TransferenciaIdTextBox.Text);
+                transferencia.TransferenciaId = id;
+                if (transferencia.Editar())
+                {
+                    HttpContext.Current.Response.Write("<SCRIPT>alert('Actualizacion Realizada')</SCRIPT>");
+                    Limpiar();
+                }
+                else
+                {
+                    HttpContext.Current.Response.Write("<SCRIPT>alert('Error al Actualizar Datos')</SCRIPT>");
+                }
+            }
+        }
+
+        protected void Eliminarbutton_Click(object sender, EventArgs e)
+        {
+            Transferencias transferencias = new Transferencias();
+            int id;
+            int.TryParse(TransferenciaIdTextBox.Text, out id);
+            transferencias.TransferenciaId = id;
+            if (transferencias.Eliminar())
+            {
+                HttpContext.Current.Response.Write("<Script>alert('Datos eliminados')</Script>");
+
+            }
+            else
+            {
+                HttpContext.Current.Response.Write("<Script>alert('Error, no se han eliminado los datos')</Script>");
+            }
+        }
+
+        protected void Buscarbutton_Click(object sender, EventArgs e)
+        {
+            Transferencias transferencia = new Transferencias();
+            int Id = 0;
+            int.TryParse(TransferenciaIdTextBox.Text, out Id);
+            transferencia.TransferenciaId = Id;
+            if (Id > 0)
+            {
+
+                if (transferencia.Buscar(Id))
+                {
+                    FechaTextBox.Text = transferencia.Fecha;
+                    CuentaOrigenTextBox.Text = transferencia.CuentaOrigenId.ToString();
+                    CuentaDestinoTextBox.Text = transferencia.CuentaDestinoId.ToString();
+                    MontoTextBox.Text = transferencia.Monto.ToString();
+                    ObservacionTextBox.Text = transferencia.Observacion;
+                    UsuarioIdTextBox.Text = transferencia.UsuarioId.ToString();
+                }
+                else
+                {
+                    HttpContext.Current.Response.Write("<SCRIPT>alert('Id No Existe')</SCRIPT>");
+                }
+            }
+            else
+            {
+                HttpContext.Current.Response.Write("<SCRIPT>alert('Id Incorrecto')</SCRIPT>");
+            }
+        }
+
+        protected void Nuevobutton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
