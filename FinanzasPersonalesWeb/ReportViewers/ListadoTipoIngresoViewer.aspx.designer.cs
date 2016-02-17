9adf9813b826188da569dd5a6d163c27ef0c522c
edwinh150 <edwinh2410@gmail.com>
2016-02-17 19:29:18 -0400

    falto esto

--- FinanzasPersonalesWeb/ReportViewers/ListadoTipoIngresoViewer.aspx.designer.cs
+++ FinanzasPersonalesWeb/ReportViewers/ListadoTipoIngresoViewer.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace FinanzasPersonalesWeb.ReportViewers {
     public partial class ListadoTipoIngresoTipoViewer {
         
         /// <summary>
-        /// ScriptManager2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.ScriptManager ScriptManager2;
-        
-        /// <summary>
         /// ReportViewer1 control.
         /// </summary>
         /// <remarks>
