7199ef7e992b2c6fdac2833f6a4af299ac48c541
angelrod25 <angelrguez25@gmail.com>
2016-02-27 00:28:31 -0400

    Esqueleto Registro de Cuentas con BootStrap y clase cuentas ready

--- BLL/Cuentas.cs
+++ BLL/Cuentas.cs
@@ -7,20 +7,21 @@ using DAL;
 
 namespace BLL
 { 
-   public  class Cuentas : ClaseMaestra
+   public  class Cuentas : ClaseMaestra 
     {
 
         public int CuentaId { get; set; }
         public int UsuarioId { get; set; }
         public string Descripcion { get; set; }
         public float Balance { get; set; }
-        public int Porciento{ get; set; }
+        public float Porciento{ get; set; }
 
         public Cuentas(){
+
             CuentaId = 0;
             UsuarioId = 0;
-            Descripcion = "";
-            Balance = 0.0f;
+            Descripcion = string.Empty;
+            Balance = 0;
             Porciento = 0;
 
         }
@@ -28,60 +29,77 @@ namespace BLL
 
         public override bool Insertar()
         {
+            ConexionDb con = new ConexionDb();
 
-            ConexionDb conexion = new ConexionDb();
             bool retorno = false;
 
-            retorno = conexion.Ejecutar(String.Format("Insert Into Cuentas (UsuarioId,Descripcion,Balance,Porciento) Values ({0},'{1}',{2},(3))", this.UsuarioId, this.Descripcion, this.Balance, this.Porciento));
+            try
+            {
+                retorno = con.Ejecutar(string.Format("insert into Cuentas (UsuarioId, Descripcion, Balance, Porciento) values ({0},'{1}',{2},{3})", this.UsuarioId, this.Descripcion, this.Balance, this.Porciento));
+            }
+            catch (Exception)
+            {
+
+                retorno = false;
+            }
+
+            return retorno;
+
+        }
+        public override bool Editar()
+
+        {
+            ConexionDb con = new ConexionDb();
+
+            bool Retorno = false;
+            Retorno = con.Ejecutar(String.Format("Update Cuentas set UsuarioId = {0}, Descripcion = '{1}', Balance = {2}, Porciento = {3} where CuentaId = {4}", this.UsuarioId, this.Descripcion, this.Balance, this.Porciento, this.CuentaId));
+            return Retorno;
+        }
+
+        public override bool Eliminar()
+        {
+            ConexionDb con = new ConexionDb();
+
+            bool retorno = false;
+            retorno = con.Ejecutar(String.Format("Delete from Cuentas where CuentaId = {0}", this.CuentaId));
             return retorno;
         }
 
         public override bool Buscar(int IdBuscado)
         {
-            ConexionDb conexion = new ConexionDb();
+            ConexionDb con = new ConexionDb();
+
             DataTable dt = new DataTable();
 
-
-            dt = conexion.ObtenerDatos(String.Format("Select * From CuentaId Where CuentaId = {0}", IdBuscado));
+            dt = con.ObtenerDatos(String.Format("Select * from Cuentas where CuentaId = {0}", IdBuscado));
             if (dt.Rows.Count > 0)
             {
                 this.CuentaId = (int)dt.Rows[0]["CuentaId"];
                 this.UsuarioId = (int)dt.Rows[0]["UsuarioId"];
                 this.Descripcion = dt.Rows[0]["Descripcion"].ToString();
-                this.Balance = (int)dt.Rows[0]["Balance"];
-                this.Porciento = (int)dt.Rows[0]["Porciento"];
+                this.Balance = (float)dt.Rows[0]["Balance"];
+                this.Porciento = (float)dt.Rows[0]["Porciento"];
+
+
             }
+
             return dt.Rows.Count > 0;
         }
 
-        public override bool Editar()
-        {
-            ConexionDb conexion = new ConexionDb();
-            bool retorno = false;
-
-            retorno = conexion.Ejecutar(String.Format("Update Cuentas Set UsuarioId = '{0}', Descripcion = {1},Balance = {2} , Porciento= {3} Where CuentaId = {4}", this.UsuarioId, this.Descripcion, this.Balance, this.Porciento, this.CuentaId));
-            return retorno;
-        }
-
-        public override bool Eliminar()
-        {
-            ConexionDb conexion = new ConexionDb();
-            bool retorno = false;
-
-            retorno = conexion.Ejecutar(String.Format("Delete Cuentas Where CuentaId = {0}", this.CuentaId));
-            return retorno;
-        }
-
-        
-
         public override DataTable Listado(string Campos, string Condicion, string Orden)
         {
-            ConexionDb conexion = new ConexionDb();
-            string ordenFinal = " ";
+            ConexionDb con = new ConexionDb();
 
+            string OrdenFinal = string.Empty;
             if (!Orden.Equals(""))
-                ordenFinal = " Orden by " + Orden;
-            return conexion.ObtenerDatos("Select " + Campos + " From Cuentas Where " + Condicion + " " + ordenFinal);
+            {
+                OrdenFinal = " Order by " + Orden;
+            }
+            return con.ObtenerDatos("Select " + Campos + "from Cuentas where " + Condicion + " " + OrdenFinal);
         }
     }
 }
+
+    
+
+        
---
+++ FinanzasPersonalesWeb/Registros/rCuentas.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FinanzasPersonalesWeb.Registros
+{
+    public partial class rCuentas : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GuardarButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LimpiarButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void EliminarButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BuscarButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
