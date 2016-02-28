55d2f74a40aa1aa0a817dae7a6897228ff1f033c
Edwin Hidalgo Lopez <edwin_hidalgo1@ucne.edu.do>
2016-02-27 22:09:57 -0400

    Creando el formulario o la web form de Ingresos con boostrap

---
+++ FinanzasPersonalesWeb/Registros/rIngresos.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="rIngresos.aspx.cs" Inherits="FinanzasPersonalesWeb.Registros.rIngresos" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <div class="panel panel-success">
+        <div class="panel-heading">Registro de Ingresos</div>
+        <div class="panel-body">
+        <div class="form-horizontal col-md-12" role="form">
+            <div class="form-group">
+
+                <%--IngresoId--%>
+                <div class="form-group">
+                    <asp:Label For="IngresoTexBox" ID="Label1" class="col-md-3 control-label input-sm" runat="server" Text="Id"></asp:Label>
+                    <div class="col-md-6 col-sm-4 col-lg-6">
+                        <asp:TextBox ID="IngresoTextBox" CssClass="form-control" runat="server" placeholder ="Escribe un id"></asp:TextBox>
+                    </div>
+                    <div class="col-md-2 col-sm-1 col-lg-2">
+                        <asp:Button For="IngresoTexBox" ID="BuscarButton" runat="server" CssClass="btn btn-primary" Text="Buscar" OnClick="BuscarButton_Click" />
+                    </div>
+                    <div class="col-md-1">
+                    </div>
+                </div>
+
+                <%--Monto--%>
+                <div class="form-group">
+                     <asp:Label For="MontoTexBox" ID="Label2" class="col-md-3 control-label input-sm" runat="server" Text="Monto"></asp:Label>
+                      <div class="col-lg-8 col-md-8">
+                          <asp:TextBox ID="MontoTexBox" CssClass="form-control" runat="server" placeholder ="Escribe un Monto"></asp:TextBox>
+                      </div>
+                      <div class="col-md-1">
+                      </div>
+                </div>
+
+                <%--Cuentas--%>
+                <div class="form-group">
+                        <asp:Label For="CuentaDropDownList" ID="Label5" class="col-md-3 control-label input-sm" runat="server" Text="Cuentas"></asp:Label>
+                        <div class="col-lg-6 col-md-6">
+                            <asp:DropDownList ID="CuentaDropDownList" CssClass="form-control" runat="server">
+                            </asp:DropDownList>
+                        </div>
+                </div>
+
+                <%--Miembro--%>
+                <div class="form-group">
+                        <asp:Label For="MiembroDropDownList" ID="Label4" class="col-md-3 control-label input-sm" runat="server" Text="Miembros"></asp:Label>
+                        <div class="col-lg-6 col-md-6">
+                            <asp:DropDownList ID="MiembroDropDownList" CssClass="form-control" runat="server">
+                            </asp:DropDownList>
+                        </div>
+                </div>
+
+                <%--TipoIngresos--%>
+                <div class="form-group">
+                        <asp:Label For="TipoIngresoDropDownList" ID="Label3" class="col-md-3 control-label input-sm" runat="server" Text="TipoIngresos"></asp:Label>
+                        <div class="col-lg-6 col-md-6">
+                            <asp:DropDownList ID="TipoIngresoDropDownList" CssClass="form-control" runat="server">
+                            </asp:DropDownList>
+                        </div>
+                </div>
+
+                <div class="col-lg-12 col-centered">
+                        <asp:Button ID="GuardarButton" CssClass="btn btn-success" runat="server" Text="Guardar" Width="101px" OnClick="GuardarButton_Click" />
+                    
+                        <asp:Button ID="LimpiarButton" CssClass="btn btn-default" runat="server" Text="Limpiar" Width="98px" OnClick="LimpiarButton_Click" />
+                    
+                        <asp:Button ID="EliminarButton" CssClass="btn btn-danger" runat="server" Text="Eliminar" Width="102px" OnClick="EliminarButton_Click" />      
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ FinanzasPersonalesWeb/Registros/rIngresos.aspx.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BLL;
+
+namespace FinanzasPersonalesWeb.Registros
+{
+    public partial class rIngresos : System.Web.UI.Page
+    {
+        TiposIngresos TipodeIngreso = new TiposIngresos();
+        Cuentas Cuenta = new Cuentas();
+        Miembros Miembro = new Miembros();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                MiembroDropDownList.DataSource = Miembro.Listado(" * ", "1=1", "");
+                MiembroDropDownList.DataTextField = "Nombres";
+                MiembroDropDownList.DataValueField = "MiembroId";
+                MiembroDropDownList.DataBind();
+
+                CuentaDropDownList.DataSource = Cuenta.Listado(" * ", "1=1", "");
+                CuentaDropDownList.DataTextField = "Descripcion";
+                CuentaDropDownList.DataValueField = "CuentaId";
+                CuentaDropDownList.DataBind();
+
+                TipoIngresoDropDownList.DataSource = TipodeIngreso.Listado(" * ", "1=1", "");
+                TipoIngresoDropDownList.DataTextField = "Descripcion";
+                TipoIngresoDropDownList.DataValueField = "TipoIngresoId";
+                TipoIngresoDropDownList.DataBind();
+            }
+        }
+
+        public int ValidarIdEntero(string IdTextBox)
+        {
+            int Id = 0;
+
+            if (IdTextBox.Length > 0)
+            {
+                bool result = Int32.TryParse(IdTextBox, out Id);
+            }
+
+            return Id;
+        }
+
+        public void Limpiar()
+        {
+            MontoTexBox.Text = "";
+            CuentaDropDownList.ClearSelection();
+            MiembroDropDownList.ClearSelection();
+            TipoIngresoDropDownList.ClearSelection();
+        }
+
+        public bool LlenarDatos()
+        {
+            bool retorno = true;
+
+            if (MontoTexBox.Text.Length > 0)
+            {
+                Ingreso.TipoIngresoId = ValidarIdEntero(TipoIngresoDropDownList.SelectedValue);
+                Ingreso.MiembroId = ValidarIdEntero(MiembroDropDownList.SelectedValue);
+                Ingreso.CuentaId = ValidarIdEntero(CuentaDropDownList.SelectedValue);
+            }
+
+            return retorno;
+        }
+
+        protected void BuscarButton_Click(object sender, EventArgs e)
+        {
+            if (ValidarIdEntero(IngresoTextBox.Text) > 0)
+            {
+                if (Ingreso.Buscar(ValidarIdEntero(IngresoTextBox.Text)))
+                {
+                    MontoTexBox.Text =
+
+                    CuentaDropDownList.SelectedValue =
+
+                    TipoIngresoDropDownList.SelectedValue =
+
+                    MiembroDropDownList.SelectedValue =
+                }
+                else
+                {
+                    Utilitarios.ShowToastr(this.Page, "No hay Registro", "Error", "Error");
+                    Limpiar();
+                }
+            }
+            else
+            {
+                Utilitarios.ShowToastr(this.Page, "Ingrese un numero Valido", "Error", "Error");
+                Limpiar();
+            }
+        }
+
+        protected void GuardarButton_Click(object sender, EventArgs e)
+        {
+            if (ValidarIdEntero(IngresoTextBox.Text) > 0)
+            {
+                if (LlenarDatos())
+                {
+                    Ingreso.IngresoId = ValidarIdEntero(IngresoTextBox.Text);
+
+                    if (Ingreso.Editar())
+                    {
+                        Utilitarios.ShowToastr(this.Page, "Se Modifico", "Felicidades", "Success");
+                        Limpiar();
+                    }
+                    else
+                    {
+                        Utilitarios.ShowToastr(this.Page, "No Modifico", "Error", "Error");
+                    }
+                }
+                else
+                {
+                    Utilitarios.ShowToastr(this.Page, "Faltan Datos", "Error", "Error");
+                }
+            }
+            else
+            {
+                if (LlenarDatos())
+                {
+                    if (Ingreso.Insertar())
+                    {
+                        Utilitarios.ShowToastr(this.Page, "Se Guardo", "Felicidades", "Success");
+                        Limpiar();
+                    }
+                    else
+                    {
+                        Utilitarios.ShowToastr(this.Page, "No Guardo", "Error", "Error");
+                    }
+                }
+                else
+                {
+                    Utilitarios.ShowToastr(this.Page, "Faltan Datos", "Error", "Error");
+                }
+            }
+        }
+
+        protected void LimpiarButton_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        protected void EliminarButton_Click(object sender, EventArgs e)
+        {
+            if (ValidarIdEntero(IngresoTextBox.Text) > 0)
+            {
+                if (Ingreso.Buscar(ValidarIdEntero(IngresoTextBox.Text)))
+                {
+                    Ingreso.IngresoId = ValidarIdEntero(IngresoTextBox.Text);
+
+                    if (Ingreso.Eliminar())
+                    {
+                        Utilitarios.ShowToastr(this.Page, "Se Elmino", "Felicidades", "Success");
+
+                        Limpiar();
+                    }
+                    else
+                    {
+                        Utilitarios.ShowToastr(this.Page, "No Elimino", "Error", "Error");
+                    }
+                }
+                else
+                {
+                    Utilitarios.ShowToastr(this.Page, "No hay Registro", "Error", "Error");
+                    Limpiar();
+                }
+            }
+            else
+            {
+                Utilitarios.ShowToastr(this.Page, "Ingrese un numero Valido", "Error", "Error");
+                Limpiar();
+            }
+        }
+    }
+}
