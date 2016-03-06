1a78bb2d8272fb95d33c69ace24bbb4037248ea4
edwinh150 <edwinh2410@gmail.com>
2016-03-06 15:44:24 -0400

    arreglando y cambiandole el nombre al viewer de tipo ingreso

---
+++ FinanzasPersonalesWeb/ReportViewers/ListadoTipoIngreso.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BLL;
+using Microsoft.Reporting.WebForms;
+
+namespace FinanzasPersonalesWeb.ReportViewers
+{
+    public partial class ListadoTipoIngreso : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                TiposIngresos TipoIngreso = new TiposIngresos();
+
+                TipoIngresoRptViewer.ProcessingMode = Microsoft.Reporting.WebForms.ProcessingMode.Local;
+                TipoIngresoRptViewer.Reset();
+                TipoIngresoRptViewer.LocalReport.ReportPath = Server.MapPath("~/Rpts/TipoIngresoRpt.rdlc");
+
+                TipoIngresoRptViewer.LocalReport.DataSources.Clear();
+
+                TipoIngresoRptViewer.LocalReport.DataSources.Add(new ReportDataSource("DataSetTipoIngreso", TipoIngreso.Listado("*", "1=1", "")));
+                TipoIngresoRptViewer.LocalReport.Refresh();
+
+            }
+        }
+    }
+}
