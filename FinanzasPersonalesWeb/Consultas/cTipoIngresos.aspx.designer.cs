b5025113edca61e1b084bb8aaee92638c44f7478
edwinh150 <edwinh2410@gmail.com>
2016-03-06 20:15:46 -0400

    terminando el reporte y agregandole query string a tipo ingreso

--- FinanzasPersonalesWeb/Consultas/cTipoIngresos.aspx.designer.cs
+++ FinanzasPersonalesWeb/Consultas/cTipoIngresos.aspx.designer.cs
@@ -49,6 +49,15 @@ namespace FinanzasPersonalesWeb.Consultas {
         protected global::System.Web.UI.WebControls.Button BuscarButton;
         
         /// <summary>
+        /// ImprimirButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ImprimirButton;
+        
+        /// <summary>
         /// ConsultaGridView control.
         /// </summary>
         /// <remarks>
